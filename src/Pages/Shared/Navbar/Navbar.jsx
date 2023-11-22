5e6df961b8bbb44e4c7ad676cce4400be2e15719
unknown <rakibulislamborkan@gmail.com>
2023-11-22 16:27:41 +0600

    hamburger dropdown menu functionality implemented

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from "../../../assets/logo.png"
 import { Link } from 'react-router-dom';
 import { IoMdShare } from "react-icons/io";
@@ -12,13 +12,30 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 
 const Navbar = () => {
+
+    const [hamClick, setHamClick] = useState(false);
+    console.log(hamClick);
+
     return (
         <>
             <div className="nav-container main-container font-montserrat font-semibold py-6 flex justify-between">
 
 
                 <div className='flex items-center gap-4'>
-                    <button className='bg-white text-black p-3 text-xl rounded-full md:hidden'><GiHamburgerMenu /></button>
+
+
+                    <div className='hamburger-menu relative'>
+                        <button onClick={() => { setHamClick(!hamClick) }} className='bg-white text-black p-3 text-xl rounded-full md:hidden'><GiHamburgerMenu /></button>
+
+                        <div className={`flex flex-col gap-2 mt-2 bg-white p-3 rounded-2xl absolute ${hamClick ? "left-[0px]" : "left-[-99999px]"} w-40`}>
+
+                            <Link className='hover:bg-gray-200 rounded-xl p-2'>About</Link>
+                            <Link className='hover:bg-gray-200 rounded-xl p-2'>Send e-gitt</Link>
+                        </div>
+
+                    </div>
+
+
                     <div className="navbar-starting md:flex items-center gap-8 hidden">
                         <img src={logo} alt="" className='w-40' />
                         <Link className='bg-white py-3 px-4 rounded-full'>About</Link>
